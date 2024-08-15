2e996f7567d856a1483b6ebc906921c1e3834a98
DongchannN <ldc991104@gmail.com>
2024-08-15 14:07:39 +0900

    first commit: Create Project Java Version : 17 Spring boot Version : 3.3.2
    [Dependency]
    spring-boot-starter-data-jpa
    spring-boot-starter-validation
    spring-boot-starter-web
    lombok
    mysql-connector-j
    // spring-boot-starter-security

---
+++ ticketing-kiwi/src/main/java/study/kiwi/ticketing_kiwi/TicketingKiwiApplication.java
@@ -0,0 +1,13 @@
+package study.kiwi.ticketing_kiwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingKiwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingKiwiApplication.class, args);
+	}
+
+}
---
+++ ticketing-kiwi/src/test/java/study/kiwi/ticketing_kiwi/TicketingKiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kiwi.ticketing_kiwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingKiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
